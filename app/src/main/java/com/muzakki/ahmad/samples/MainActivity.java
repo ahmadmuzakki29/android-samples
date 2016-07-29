c066bbb04b4939aeac262dd9385e1233bb8dceef
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2016-07-29 18:34:02 +0700

    init sample

---
+++ app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
@@ -0,0 +1,18 @@
+package com.muzakki.ahmad.samples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.muzakki.ahmad.lib.Constant;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txt = (TextView) findViewById(R.id.txtTest);
+        txt.setText(Constant.HOST);
+    }
+}
