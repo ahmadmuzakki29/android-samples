d9ec7a19a1d6c8be065a5d300f63b3dd2aef0ae5
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2016-08-01 08:20:33 +0700

    main activity

--- app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
+++ app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.muzakki.ahmad.lib.Constant;
+import com.muzakki.ahmad.helper.Constant;
 
 public class MainActivity extends AppCompatActivity {
 
