d374a6b0b38c3fba45cb6136477355f6941e060f
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2016-08-03 14:04:09 +0700

    add justify layout

--- app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
+++ app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
@@ -1,18 +1,38 @@
 package com.muzakki.ahmad.samples;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.util.Log;
 
-import com.muzakki.ahmad.helper.Constant;
+import com.muzakki.ahmad.material.dashboard.DashboardActivity;
+import com.muzakki.ahmad.material.dashboard.DashboardItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends DashboardActivity {
+
+    private ArrayList<DashboardItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView txt = (TextView) findViewById(R.id.txtTest);
-        txt.setText(Constant.HOST);
+        items = new ArrayList<>();
+        items.add(new DashboardItem("Menu 1",R.drawable.picture));
+        items.add(new DashboardItem("Menu 2",R.drawable.picture));
+        items.add(new DashboardItem("Menu 3",R.drawable.picture));
+        items.add(new DashboardItem("Menu 4",R.drawable.picture));
+        items.add(new DashboardItem("Menu 5",R.drawable.picture));
+        render();
     }
+
+    @Override
+    protected ArrayList<DashboardItem> getItems() {
+        return items;
+    }
+
+    @Override
+    protected void onMenuClick(int i) {
+        Log.i("jeki","clicked:"+i);
+    }
+
+
 }
