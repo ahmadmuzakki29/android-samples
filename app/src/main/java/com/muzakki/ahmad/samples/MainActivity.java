81b1ff7af237e8783ac99a5465edf8835b0526f8
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2016-08-07 07:29:48 +0700

    fix submodule

--- app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
+++ app/src/main/java/com/muzakki/ahmad/samples/MainActivity.java
@@ -1,38 +1,27 @@
 package com.muzakki.ahmad.samples;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.Fragment;
 
 import com.muzakki.ahmad.material.dashboard.DashboardActivity;
-import com.muzakki.ahmad.material.dashboard.DashboardItem;
-
-import java.util.ArrayList;
+import com.muzakki.ahmad.material.dashboard.HomeFragment;
 
 public class MainActivity extends DashboardActivity {
-
-    private ArrayList<DashboardItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        items = new ArrayList<>();
-        items.add(new DashboardItem("Menu 1",R.drawable.picture));
-        items.add(new DashboardItem("Menu 2",R.drawable.picture));
-        items.add(new DashboardItem("Menu 3",R.drawable.picture));
-        items.add(new DashboardItem("Menu 4",R.drawable.picture));
-        items.add(new DashboardItem("Menu 5",R.drawable.picture));
-        render();
+
     }
 
     @Override
-    protected ArrayList<DashboardItem> getItems() {
-        return items;
+    protected String[] getMenuItems() {
+        return new String[]{"Home","Profile"};
     }
 
     @Override
-    protected void onMenuClick(int i) {
-        Log.i("jeki","clicked:"+i);
+    protected Fragment getFragmentInstance(int pos) {
+        return HomeFragment.newInstance();
     }
-
 
 }
